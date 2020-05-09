422d3bce38b3e597e0de7eb2b195fd67992b5aa0
Itamar Soffer <itamar.soffer@gmail.com>
2020-05-09 14:43:05 +0300

    initial commit for client

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Timeline from './timeline';
+import Home from './ReactHome';
+import EventForm from './addevent';
+import LoginForm from './login/login_form';
 import './App.css';
+import 'antd/dist/antd.css';
+import './login/adds_atnd.css'
+import './main.css' // ??
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	<Router>
+
+	<div>
+      {/*comment the nav to hide this*/}
+		<nav>
+		<ul>
+			<li>
+				<Link to="/"> Home </Link>
+			</li>
+			<li>
+				<Link to="/timeline"> Timeline </Link>
+			</li>
+			<li>
+				<Link to="/add"> Add new</Link>
+			</li>
+			<li>
+				<Link to="/login"> Login Form</Link>
+			</li>
+		</ul>
+		</nav>
+
+	<Switch>
+
+		<Route path="/timeline">
+			<Timeline />
+		</Route>
+
+		<Route path="/add">
+		<EventForm />
+		</Route>
+
+		<Route path="/login">
+		<LoginForm />
+		</Route>
+
+		<Route path= "/">
+			<Home />
+		</Route>
+
+	</Switch>
+
+	</div>
+	</Router>
+);
+
 }
 
 export default App;
