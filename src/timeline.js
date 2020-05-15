32e7061628726c5d1e6f3b6ebf5d70d298398b3f
Itamar Soffer <itamar.soffer@gmail.com>
2020-05-15 17:17:05 +0300

    add API connection and DataEvent component use

--- src/timeline.js
+++ src/timeline.js
@@ -8,134 +8,189 @@ import {
 import {
   AiFillStar as StarIcon,
 } from "react-icons/ai";
-
+import axios from 'axios';
 import {Tag} from 'antd';
+import DataEvent from './dataEvent/dataEventComponent'
 
+
+
+const base_url = "http://itsoffer:5005/api/timeline/";
+const data1 = {
+    background: 'rgb(255, 255, 255)',
+    date: "2011 - present",
+    iconBackground: 'rgb(33, 150, 243)',
+    iconColor: '#fff',
+    icon: <WorkIcon />,
+    h3: "Creative Director",
+    h4: "Miami, FL",
+    p: "check my dataEvent component"
+};
 
 class Timeline extends React.Component {
 
+    constructor(props){
+        super(props);
+        this.state={
+            isLoaded: false,
+            timeline_events: []
+        }
+    }
+
+    componentDidMount() {
+        const TimelineUrl = this.props.match.params.timeline_url;
+        const url = base_url.concat(TimelineUrl);
+        console.log(url);
+        axios.get(url)
+            .then(res => res.data.events)
+            .then((evs) => {
+                this.setState({
+                    timeline_events:evs,
+                    isLoaded: true})})
+                .then(() => {console.log("StateEvents:", this.state.timeline_events)});
+    }
+
+
+
     render() {
-        return (
-            <div
-                  style={{
-                    backgroundColor: '#ccc',
-                  }}>
-        <VerticalTimeline
-            style={{background: '#f00'}}>
-          <VerticalTimelineElement
-            className="vertical-timeline-element--work"
-            contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-            contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
-            date="2011 - present"
-            iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-            icon={<WorkIcon />}
+        if (!this.state.isLoaded) {
+            // if Not Loaded:
 
-          >
-            <h3 className="vertical-timeline-element-title">Creative Director</h3>
-            <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
-            <p>
-              Creative Direction, User Experience, Visual Design, Project Management, Team Leading
-            </p>
-          </VerticalTimelineElement>
-          <VerticalTimelineElement
-            className="vertical-timeline-element--work"
-            date="2010 - 2011"
-            iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-            icon={<WorkIcon />}
+            return <div>Loading</div>;
+        }
+        else
+            {
+                //returns the timeline.
 
-          >
-            <h3 className="vertical-timeline-element-title">Art Director</h3>
-            <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
-            <p>
-              Creative Direction, User Experience, Visual Design, SEO, Online Marketing
-            </p>
-          </VerticalTimelineElement>
-          <VerticalTimelineElement
-            className="vertical-timeline-element--work"
-            date="2008 - 2010"
-            iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-            icon={<WorkIcon />}
-          >
-            <h3 className="vertical-timeline-element-title">Web Designer</h3>
-            <h4 className="vertical-timeline-element-subtitle">Los Angeles, CA</h4>
-            <p>
-              User Experience, Visual Design
-            </p>
-          </VerticalTimelineElement>
-          <VerticalTimelineElement
-            className="vertical-timeline-element--work"
-            date="2006 - 2008"
-            iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-            icon={<WorkIcon />}
-          >
-            <h3 className="vertical-timeline-element-title">Web Designer</h3>
-            <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
-            <p>
-              User Experience, Visual Design
-            </p>
-          </VerticalTimelineElement>
-          <VerticalTimelineElement
-            className="vertical-timeline-element--education"
-            date="April 2013"
-            iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-            icon={<SchoolIcon />}
-          >
-            <h3 className="vertical-timeline-element-title">Content Marketing for Web, Mobile and Social Media</h3>
-            <h4 className="vertical-timeline-element-subtitle">Online Course</h4>
-            <p>
-              Strategy, Social Media
-            </p>
-          </VerticalTimelineElement>
-          <VerticalTimelineElement
-            className="vertical-timeline-element--education"
-            date="November 2012"
-            iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-            icon={<SchoolIcon />}
-          >
-            <h3 className="vertical-timeline-element-title">Agile Development Scrum Master</h3>
-            <h4 className="vertical-timeline-element-subtitle">Certification</h4>
-            <p>
-              Creative Direction, User Experience, Visual Design
-            </p>
-          </VerticalTimelineElement>
-          <VerticalTimelineElement
-            className="vertical-timeline-element--education"
-            date="2002 - 2006"
-            iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-            icon={<SchoolIcon />}
-          >
-            <h3 className="vertical-timeline-element-title">Bachelor of Science in Interactive Digital Media Visual Imaging</h3>
-            <h4 className="vertical-timeline-element-subtitle">Bachelor Degree</h4>
-            <p>
-              Creative Direction, Visual Design
-            </p>
-          </VerticalTimelineElement>
-          <VerticalTimelineElement
-            className="vertical-timeline-element--education"
-            date="2002 - 2006"
-            iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-            icon={<SchoolIcon />}
-          >
-            <Tag color="magenta"> magenta </Tag>
-            <Tag color="purple" closable={true}> purple </Tag>
-            <h1 className="vertical-timeline-element-title">האם זה יעשה עברית יפה? כותרת 1</h1>
-            <h2 className="vertical-timeline-element-title">האם זה יעשה עברית יפה? כותרת 2</h2>
-            <h3 className="vertical-timeline-element-title">האם זה יעשה עברית יפה? כותרת 3</h3>
-            <h4 className="vertical-timeline-element-subtitle">ומה יקרה פה? כותרת 4</h4>
-            <p>
-              Creative Direction, Visual Design
-            </p>
-          </VerticalTimelineElement>
-          <VerticalTimelineElement
-            iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
-            icon={<StarIcon />}
-          />
-
-        </VerticalTimeline>
-              </div>
+            return (
+                <div
+                    style={{
+                        backgroundColor: '#ccc',
+                    }}>
+                    <h2>{this.props.match.params.timeline_url}</h2>
 
 
-        );
+                    <VerticalTimeline
+                        style={{background: '#f00'}}>
+                        <VerticalTimelineElement
+                            className="vertical-timeline-element--work"
+                            contentStyle={{background: 'rgb(33, 150, 243)', color: '#fff'}}
+                            contentArrowStyle={{borderRight: '7px solid  rgb(33, 150, 243)'}}
+                            date="2011 - present"
+                            iconStyle={{background: 'rgb(33, 150, 243)', color: '#fff'}}
+                            icon={<WorkIcon/>}
+
+                        >
+                            <h3 className="vertical-timeline-element-title">Creative Director</h3>
+                            <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
+                            <p>
+                                Creative Direction, User Experience, Visual Design, Project Management, Team Leading
+                            </p>
+                        </VerticalTimelineElement>
+                        <VerticalTimelineElement
+                            className="vertical-timeline-element--work"
+                            date="2010 - 2011"
+                            iconStyle={{background: 'rgb(33, 150, 243)', color: '#fff'}}
+                            icon={<WorkIcon/>}
+
+                        >
+                            <h3 className="vertical-timeline-element-title">Art Director</h3>
+                            <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
+                            <p>
+                                Creative Direction, User Experience, Visual Design, SEO, Online Marketing
+                            </p>
+                        </VerticalTimelineElement>
+                        <VerticalTimelineElement
+                            className="vertical-timeline-element--work"
+                            date="2008 - 2010"
+                            iconStyle={{background: 'rgb(33, 150, 243)', color: '#fff'}}
+                            icon={<WorkIcon/>}
+                        >
+                            <h3 className="vertical-timeline-element-title">Web Designer</h3>
+                            <h4 className="vertical-timeline-element-subtitle">Los Angeles, CA</h4>
+                            <p>
+                                User Experience, Visual Design
+                            </p>
+                        </VerticalTimelineElement>
+                        <VerticalTimelineElement
+                            className="vertical-timeline-element--work"
+                            date="2006 - 2008"
+                            iconStyle={{background: 'rgb(33, 150, 243)', color: '#fff'}}
+                            icon={<WorkIcon/>}
+                        >
+                            <h3 className="vertical-timeline-element-title">Web Designer</h3>
+                            <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
+                            <p>
+                                User Experience, Visual Design
+                            </p>
+                        </VerticalTimelineElement>
+                        <VerticalTimelineElement
+                            className="vertical-timeline-element--education"
+                            date="April 2013"
+                            iconStyle={{background: 'rgb(233, 30, 99)', color: '#fff'}}
+                            icon={<SchoolIcon/>}
+                        >
+                            <h3 className="vertical-timeline-element-title">Content Marketing for Web, Mobile and Social
+                                Media</h3>
+                            <h4 className="vertical-timeline-element-subtitle">Online Course</h4>
+                            <p>
+                                Strategy, Social Media
+                            </p>
+                        </VerticalTimelineElement>
+                        <VerticalTimelineElement
+                            className="vertical-timeline-element--education"
+                            date="November 2012"
+                            iconStyle={{background: 'rgb(233, 30, 99)', color: '#fff'}}
+                            icon={<SchoolIcon/>}
+                        >
+                            <h3 className="vertical-timeline-element-title">Agile Development Scrum Master</h3>
+                            <h4 className="vertical-timeline-element-subtitle">Certification</h4>
+                            <p>
+                                Creative Direction, User Experience, Visual Design
+                            </p>
+                        </VerticalTimelineElement>
+                        <VerticalTimelineElement
+                            className="vertical-timeline-element--education"
+                            date="2002 - 2006"
+                            iconStyle={{background: 'rgb(233, 30, 99)', color: '#fff'}}
+                            icon={<SchoolIcon/>}
+                        >
+                            <h3 className="vertical-timeline-element-title">Bachelor of Science in Interactive Digital
+                                Media Visual Imaging</h3>
+                            <h4 className="vertical-timeline-element-subtitle">Bachelor Degree</h4>
+                            <p>
+                                Creative Direction, Visual Design
+                            </p>
+                        </VerticalTimelineElement>
+                        <VerticalTimelineElement
+                            className="vertical-timeline-element--education"
+                            date="2002 - 2006"
+                            iconStyle={{background: 'rgb(233, 30, 99)', color: '#fff'}}
+                            icon={<SchoolIcon/>}
+                        >
+                            <Tag color="magenta"> magenta </Tag>
+                            <Tag color="purple" closable={true}> purple </Tag>
+                            <h1 className="vertical-timeline-element-title">האם זה יעשה עברית יפה? כותרת 1</h1>
+                            <h2 className="vertical-timeline-element-title">האם זה יעשה עברית יפה? כותרת 2</h2>
+                            <h3 className="vertical-timeline-element-title">האם זה יעשה עברית יפה? כותרת 3</h3>
+                            <h4 className="vertical-timeline-element-subtitle">ומה יקרה פה? כותרת 4</h4>
+                            <p>
+                                Creative Direction, Visual Design
+                            </p>
+                        </VerticalTimelineElement>
+
+                        <VerticalTimelineElement
+                            iconStyle={{background: 'rgb(16, 204, 82)', color: '#fff'}}
+                            icon={<StarIcon/>}
+                        />
+                        <DataEvent data={data1}/>
+
+
+                    </VerticalTimeline>
+                </div>
+
+
+            );
+        }
     }
 }
 
