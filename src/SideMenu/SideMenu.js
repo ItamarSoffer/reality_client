c2e4e6adce1314fbb2cd612ac7264f70eb0df772
Itamar Soffer <itamar.soffer@gmail.com>
2020-05-16 12:38:44 +0300

    small fix

--- src/SideMenu/SideMenu.js
+++ src/SideMenu/SideMenu.js
@@ -8,8 +8,13 @@ import {
   UserOutlined,
 } from '@ant-design/icons';
 
+import MenuIcons from '../Icons/MenuIcons';
+
+
 const { Header, Content, Footer, Sider } = Layout;
 const { SubMenu } = Menu;
+
+
 
 class SideMenu extends React.Component {
   state = {
@@ -32,18 +37,12 @@ class SideMenu extends React.Component {
   };
 
 
-
-
   render() {
     return (
         <Sider collapsible
                collapsed={this.state.collapsed}
                onCollapse={this.onCollapse}
                style={{background: this.state.menuBackground,
-
-
-
-
       }}
         >
           <div className="logo" />
@@ -52,18 +51,33 @@ class SideMenu extends React.Component {
               mode="inline"
               defaultSelectedKeys={['1']}
           >
-            <Menu.Item key="1" icon={<PieChartOutlined />}>
-              Option 1
-            </Menu.Item>
-            <Menu.Item key="2" icon={<DesktopOutlined />}>
-              Option 2
-            </Menu.Item>
-            <SubMenu key="sub1" icon={<UserOutlined />} title="User">
+            <SubMenu key="sub1" icon={MenuIcons['user']} title="Development">
               <Menu.Item key="3">Tom</Menu.Item>
               <Menu.Item key="4">Bill</Menu.Item>
               <Menu.Item key="5">Alex</Menu.Item>
             </SubMenu>
-            <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
+            <Menu.Item key="my_timelines" icon={MenuIcons['piechart']}>
+              My Timelines
+            </Menu.Item>
+            <Menu.Item key="Home" icon={MenuIcons['piechart']}>
+              Home
+            </Menu.Item>
+            <Menu.Item key="Logout" icon={MenuIcons['piechart']}>
+              Logout
+            </Menu.Item>
+
+            <Menu.Item key="1" icon={MenuIcons['piechart']}>
+              Option 1
+            </Menu.Item>
+            <Menu.Item key="2" icon={MenuIcons['desktop']}>
+              Option 2
+            </Menu.Item>
+            <SubMenu key="sub1" icon={MenuIcons['user']} title="User">
+              <Menu.Item key="3">Tom</Menu.Item>
+              <Menu.Item key="4">Bill</Menu.Item>
+              <Menu.Item key="5">Alex</Menu.Item>
+            </SubMenu>
+            <SubMenu key="sub2" icon={MenuIcons['team']} title="Team">
               <Menu.Item key="6">Team 1</Menu.Item>
               <Menu.Item key="8">Team 2</Menu.Item>
             </SubMenu>
