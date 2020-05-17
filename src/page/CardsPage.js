cb5125ee72a0d16620cdcd0b368139cc377fbd35
Itamar Soffer <itamar.soffer@gmail.com>
2020-05-17 23:33:49 +0300

    connected to API

--- src/page/CardsPage.js
+++ src/page/CardsPage.js
@@ -2,25 +2,38 @@ import React from 'react';
 import SideMenu from '../SideMenu/SideMenu';
 import {Layout} from "antd";
 import CardsGrid from '../RealityCard/CardsGrid'
+import axios from "axios";
 
-const cardsData =[
-    {"name": "my test- ציר זמן ראשון",
-    "url": "base",
-    "id": "2525",
-    "create_user":"itamar"},
-        {"name": "my test- ציר זמן שני- החיים ",
-    "url": "soffer",
-    "id": "2305",
-    "create_user":"itamar"},
 
-];
+const base_timelines_url = "http://itsoffer:5005/api/get_all_names";
+
 class CardsPage extends  React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            isLoaded: false,
+            timelines: []
+        }
+    }
+
+    componentDidMount() {
+        console.log(base_timelines_url);
+        axios.get(base_timelines_url)
+            .then(res => res.data)
+            .then((data) => {
+                this.setState({
+                    timelines:data,
+                    isLoaded: true})})
+                .then(() => {console.log("Timelines:", this.state.timelines)});
+    }
+
+
     render() {
         return(
         <Layout style={{ minHeight: '100vh' }} >
                           <SideMenu />
                   <Layout>
-                      <CardsGrid cardsList={cardsData}/>
+                      <CardsGrid cardsList={this.state.timelines}/>
                   </Layout>
         </Layout>
         )
