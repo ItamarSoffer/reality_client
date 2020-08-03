ffa292b35043683a4c9680efb09ecafb3d2090a4
Itamar Soffer <itamar.soffer@gmail.com>
2020-08-03 08:14:59 +0300

    add inner search to permission table

--- src/Components/permissionsModal/permissionsTable.js
+++ src/Components/permissionsModal/permissionsTable.js
@@ -1,8 +1,22 @@
 import React from "react";
 import axios from 'axios'
-import {Table, Tag} from "antd";
+import {Button, ConfigProvider, Input, Space, Table, Tag} from "antd";
 import {backendAPI} from "../../Structure/api";
 import {connect} from "react-redux";
+import Highlighter from "react-highlight-words";
+import { SearchOutlined } from '@ant-design/icons';
+import {Typography} from "antd";
+const {Paragraph} = Typography;
+
+
+function handleText (text){
+    return <ConfigProvider direction={"rtl"} >
+                <Paragraph style={{whiteSpace: "pre-line", textAlign: 'center'}}>
+                    {text}
+                </Paragraph>
+            </ConfigProvider>
+
+}
 
 
 const colormap = {
@@ -12,33 +26,6 @@ const colormap = {
     'creator': 'black'
 };
 
-
-const columns = [
-      {
-    title: 'Username',
-    dataIndex: 'username',
-    key: 'username',
-  },
-    {
-    title: 'Name',
-    dataIndex: 'display_name',
-    key: 'display_name',
-  },
-  {
-    title: 'Role',
-    dataIndex: 'role',
-    key: 'role',
-      render:  tag => (
-      <>{
-
-          <Tag color={colormap[tag]} key={tag}>
-              {tag.toUpperCase()}
-          </Tag>
-      }
-
-
-      </> )
-  }];
 
 class PermissionsTable extends React.Component{
     state={
@@ -60,6 +47,104 @@ class PermissionsTable extends React.Component{
             })
 
     }
+    setColumns() {
+
+        const columns = [
+          {
+        title: 'Username',
+        dataIndex: 'username',
+        key: 'username',
+              ...this.getColumnSearchProps('username'),
+
+        },
+        {
+        title: 'Name',
+        dataIndex: 'display_name',
+        key: 'display_name',
+                        ...this.getColumnSearchProps('display_name'),
+
+      },
+      {
+        title: 'Role',
+        dataIndex: 'role',
+        key: 'role',
+          render:  tag => (
+          <>{
+
+              <Tag color={colormap[tag]} key={tag}>
+                  {tag.toUpperCase()}
+              </Tag>
+          }
+
+
+          </> )
+      }];
+        return columns;
+    }
+
+    getColumnSearchProps = dataIndex => ({
+    filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }) => (
+      <div style={{ padding: 8 }}>
+        <Input
+          ref={node => {
+            this.searchInput = node;
+          }}
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={e => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+          onPressEnter={() => this.handleSearch(selectedKeys, confirm, dataIndex)}
+          style={{ width: 188, marginBottom: 8, display: 'block' }}
+        />
+        <Space>
+          <Button
+            type="primary"
+            onClick={() => this.handleSearch(selectedKeys, confirm, dataIndex)}
+            icon={<SearchOutlined />}
+            size="small"
+            style={{ width: 90 }}
+          >
+            Search
+          </Button>
+          <Button onClick={() => this.handleReset(clearFilters)} size="small" style={{ width: 90 }}>
+            Reset
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: filtered => <SearchOutlined style={{ color: filtered ? '#1890ff' : undefined }} />,
+    onFilter: (value, record) =>
+      record[dataIndex] === null ? false: record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
+    onFilterDropdownVisibleChange: visible => {
+      if (visible) {
+        setTimeout(() => this.searchInput.select());
+      }
+    },
+    render: text =>
+      this.state.searchedColumn === dataIndex ?
+        <Highlighter
+          highlightStyle={{ backgroundColor: '#ffc069', padding: 0 }}
+          searchWords={[this.state.searchText]}
+          autoEscape
+          textToHighlight={text.toString()}
+        />
+       :
+        handleText(text)
+      ,
+  });
+
+    handleSearch = (selectedKeys, confirm, dataIndex) => {
+    confirm();
+    this.setState({
+      searchText: selectedKeys[0],
+      searchedColumn: dataIndex,
+    });
+  };
+
+    handleReset = clearFilters => {
+    clearFilters();
+    this.setState({ searchText: '' });
+    };
+
 
     componentWillMount() {
         this.setState({
@@ -83,6 +168,7 @@ class PermissionsTable extends React.Component{
             return( <p> not loaded</p>)
         }
         else {
+            const columns = this.setColumns();
 
 
         return(
