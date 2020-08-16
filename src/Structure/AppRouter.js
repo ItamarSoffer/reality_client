6771aa4291f35005d7746d2a1062cb786242e844
Itamar Soffer <itamar.soffer@gmail.com>
2020-08-16 17:53:33 +0300

    delete non relevant routes

--- src/Structure/AppRouter.js
+++ src/Structure/AppRouter.js
@@ -5,11 +5,8 @@ import {
     Route,
 } from "react-router-dom";
 import LoginPage from "../Pages/LoginPage";
-import SideMenuDemo from "../Components/SideMenu/OriginalSideMenu";
-import BaseTimeline from "../Components/Timelines/base_timeline";
 import StoryPage from "../Pages/TimelinePage";
 import CardsPage from "../Pages/CardsPage";
-import CreateNewEvent from "../Components/NewEvent/NewEventComponent";
 import NewTimelinePage from "../Pages/NewTimelinePage";
 import HomePage from "../Pages/HomePage";
 
@@ -27,33 +24,21 @@ class AppRouter extends React.Component{
                           !this.props.isLogged ?
 
                               <div>
-
-
-                                  {/*<Route exact={true} path="/login" component={LoginPage}/>*/}
                                   <Route path="/" component={LoginPage}/>
                               </div> :
                               <div>
-
-                                  < Route path="/side_menu" component={SideMenuDemo}/>
-
-                                  <Route path="/base_timeline" component={BaseTimeline}/>
 
                                   <Route path="/timeline/:timeline_url" component={StoryPage}/>
 
                                   <Route path="/all" component={CardsPage}/>
 
-                                  <Route path="/new_event" component={CreateNewEvent}/>
-
                                   <Route path="/new_story" component={NewTimelinePage}/>
 
-                                  {/*<Route path="/login" component={LoginPage}/>*/}
                                   <Route path="/login" component={LoginPage}/>
 
                                   <Route exact={true} path="/home" component={HomePage}/>
                                   <Route exact={true} path="/" component={HomePage}/>
 
-                                  {/*<Route exact={true} path="/home" component={CardsPage}/>*/}
-                                  {/*<Route exact={true} path="/" component={CardsPage}/>*/}
                               </div>
 
                       }
