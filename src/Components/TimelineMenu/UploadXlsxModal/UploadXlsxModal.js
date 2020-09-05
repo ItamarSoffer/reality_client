d630940dfedbf296edaa6ddf5a20d41e446a3f9c
Itamar Soffer <itamar.soffer@gmail.com>
2020-09-05 11:46:48 +0300

    tags, tags search

--- src/Components/TimelineMenu/UploadXlsxModal/UploadXlsxModal.js
+++ src/Components/TimelineMenu/UploadXlsxModal/UploadXlsxModal.js
@@ -89,6 +89,7 @@ class UploadXlsxModal extends React.Component {
                <Paragraph>
                    <ul>
                        <li>xlsx file, not csv.</li>
+<<<<<<< HEAD
                        <li>First line is headers</li>
                        <li>The Headers are: title, content, link, event_time, color, icon.</li>
                        <li><Text strong>Required headers: title, event_time.</Text></li>
@@ -96,6 +97,16 @@ class UploadXlsxModal extends React.Component {
                        <li>Time format: YYYY-mm-dd HH:MM:SS</li>
                        <li>color is text in format: "rgb(233, 30, 99)" -(Red, Green, Blue), 1-255 </li>
                        <li>Multi sheets is optional, only if they have the same headers.</li>
+=======
+                       <li>First line is headers.</li>
+                       <li>The headers are: title, content, link, event_time, color, icon, tags, create_user.</li>
+                       <li><Text strong>Required headers: title, event_time.</Text></li>
+                       <li>aAl headers in small letters.</li>
+                       <li>Time format: "YYYY-mm-dd HH:MM:SS".</li>
+                       <li>Color is text in format: "rgb(233, 30, 99)" or "#FFFFFF".</li>
+                       <li>Tags: separated by commas.</li>
+                       <li>Multi sheets is optional- only if they have the same headers.</li>
+>>>>>>> ecf32fa... can upload tags
                    </ul>
                </Paragraph>
 
@@ -121,9 +132,6 @@ const mapStateToProps = state => {
   return {
       jwtToken: state.usersReducer.jwtToken,
       showUploadXlsxModal: state.modalsReducer.showUploadXlsxModal,
-
-
-
   }
 };
 
