601d65d9cdd86abaefcf427c97f0d7a4a85f4906
Itamar Soffer <itamar.soffer@gmail.com>
2020-09-08 10:29:54 +0300

    more to the about

--- src/Components/AboutModal/FeaturesCollapse.js
+++ src/Components/AboutModal/FeaturesCollapse.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import {Collapse} from "antd";
+import {Collapse, ConfigProvider,Typography, Image, Tag} from "antd";
 
 const {Panel} = Collapse;
+const {Paragraph} = Typography;
 /*
 
                         - פתיחת ציר
@@ -16,19 +17,43 @@ const {Panel} = Collapse;
 
  */
 
+function handleHebText (text){
+    return <ConfigProvider direction={"rtl"} >
+        <Paragraph style={{whiteSpace: "pre-line"}}>
+            {text}
+        </Paragraph>
+    </ConfigProvider>
+
+}
+
+function handleEnText (text){
+    return <Paragraph style={{whiteSpace: "pre-line"}}>
+            {text}
+        </Paragraph>
+
+}
+
 class FeaturesCollapse extends React.Component {
 
     render() {
         const text = '1111';
         return (
-            <Collapse>
-                <Panel header="What is Story" key="0">
+
+            <Collapse defaultActiveKey={['1', '2', '3', '8']}>
+                <Panel header="What is Story" key="0" >
                     כל החפירה שכתבתי
                 </Panel>
                 <Panel header="Basics" key="1">
-                    - Create story
-                    - add events
-                    - editing
+                    {
+                        handleEnText(
+                            "- Create story\n" +
+                            "                    - add events\n" +
+                            "                    - editing\n" +
+                            "                    V- EDITING OPTIONS IMAGE"
+                        )
+                    }
+
+                    <Image src={require('./images/EditOptions.png')} width={120}/>
                 </Panel>
                 <Panel header="Tags" key="2">
                     <p>{text}</p>
@@ -36,18 +61,44 @@ class FeaturesCollapse extends React.Component {
                     - add to events
                     - edit tags
                     - delete tags
+                    V - TAGS IMAGE
+                    <Image src={require('./images/TagsClosable.png')} width={200}/>
+
 
                 </Panel>
                 <Panel header="Filters" key="3">
-                    <p>{text}</p>
-                    - filter by text
-                    - filter by time
-                    - filter by tags
-                    - saved in url
+                    {handleEnText(
+                    `Story Lets you to filter by the following parameters:
+                    1. Time: Select a Time range of the events.
+                    2. Text: Searches in title, content and link.
+                    3. Tags: Matches all the events with al least one of the selected tags.
+                    The filter will be saved in the URL, so it can be copied.
+                    `)}
+
+                    <Image src={require('./images/StoryFilters.png')} width={215} height={95}/>
                 </Panel>
                 <Panel header="Permissions" key="4">
-                    <p>{text}</p>
-                    - permissions levels
+                    {handleEnText(
+                        `Each story has an inner permission control.
+                        There are 4 permission roles:
+                        1. Read: Only read the story, can not edit.
+                        2. Write: Read + Edit story.
+                        3. Owner:  Write + add permissions to other users. Can delete the whole story. 
+                        4. Creator: The user who created the story. These permissions can not be given by a user.
+                    `)}
+                      <Tag color={'geekblue'} key={'read'}>
+                            {'READ'}
+                        </Tag>
+                    <Tag color={'volcano'} key={'write'}>
+                            {'WRITE'}
+                        </Tag>
+                    <Tag color={'red'} key={'owner'}>
+                            {'OWNER'}
+                        </Tag>
+                    <Tag color={'black'} key={'creator'}>
+                            {'CREATOR'}
+                        </Tag>
+
                 </Panel>
                 <Panel header="Modes & Views" key="5">
                     <Collapse defaultActiveKey="5_1">
@@ -55,9 +106,9 @@ class FeaturesCollapse extends React.Component {
                             - Timeline view
                             - table view
                         </Panel>
-                            <Panel header="Views" key="5_2">
-                                - expand
-                                - compress
+                        <Panel header="Views" key="5_2">
+                            - expand
+                            - compress
                         </Panel>
                     </Collapse>
                 </Panel>
@@ -65,6 +116,7 @@ class FeaturesCollapse extends React.Component {
                     - Import xlsx
                     - Export xlsx
                     - example table?
+                    - import and export options IMAGE
                     <p>{text}</p>
                 </Panel>
                 <Panel header="Integrations" key="7">
@@ -76,6 +128,8 @@ class FeaturesCollapse extends React.Component {
                     - Adding to favorites
                     - view in side.
                     <p>{text}</p>
+                    - favorites image
+                    <Image src={require('./images/Favorites.png')}/>
                 </Panel>
             </Collapse>
         )
